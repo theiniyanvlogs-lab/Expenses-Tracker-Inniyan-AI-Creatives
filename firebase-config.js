ae9671a1fa56397859925e55f721be4336bffdf4
theiniyanvlogs-lab <the.iniyan.vlogs@gmail.com>
2026-05-08 13:34:30 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@
 // Follow setup instructions in README.md to get your config
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY_HERE",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCLZf14OnUei2bG7AGbNSGGp_YAKxAPF24",
+  authDomain: "exptraciac.firebaseapp.com",
+  projectId: "exptraciac",
+  storageBucket: "exptraciac.firebasestorage.app",
+  messagingSenderId: "472650772751",
+  appId: "1:472650772751:web:2547c1de646d8f67ae0e7a"
 };
 
 // Initialize Firebase
